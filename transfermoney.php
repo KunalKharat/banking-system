5ec089a044d9a0cf7f6f1338558b71733fa9c697
Kunal Kharat <55823981+KunalKharat@users.noreply.github.com>
2021-03-19 12:41:35 +0530

    Update transfermoney.php

--- transfermoney.php
+++ transfermoney.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
     <link rel="stylesheet" href="css/style.css">
-  <title>Team Spark Foundation Bank</title>
+  <title>The Spark Foundation Bank</title>
   <link rel="shortcut icon" href="images/logo.png" type="image/x-icon">
   <script src="https://kit.fontawesome.com/c8e4d183c2.js" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="css/table.css">
